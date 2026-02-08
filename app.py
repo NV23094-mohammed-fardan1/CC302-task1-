2852b9b98906da3d2b1021acc55e286971e6a555
NV23094-mohammed-fardan1 <NV23094@NCST.EDU.BH>
2026-02-08 08:23:45 +0000

    bootstrap code update

--- app.py
+++ app.py
@@ -1,64 +1,54 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-
-# Database config
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-# -------------------------
 # Database Model
-# -------------------------
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     task = db.Column(db.String(200), nullable=False)
     completed = db.Column(db.Boolean, default=False)
 
-# Create DB
-with app.app_context():
-    db.create_all()
-
-# -------------------------
-# READ
-# -------------------------
+# Home Page
 @app.route('/')
 def index():
     tasks = Todo.query.all()
-    return render_template('index.html', tasks=tasks)
 
-# -------------------------
-# CREATE
-# -------------------------
+    total = len(tasks)
+    completed = len([t for t in tasks if t.completed])
+
+    return render_template(
+        'index.html',
+        tasks=tasks,
+        total=total,
+        completed=completed
+    )
+
+# Add Task
 @app.route('/add', methods=['POST'])
 def add():
-    task_text = request.form['task']
-    new_task = Todo(task=task_text)
+    task = request.form['task']
+    new_task = Todo(task=task)
 
     db.session.add(new_task)
     db.session.commit()
 
-    return redirect(url_for('index'))
+    return redirect('/')
 
-# -------------------------
-# UPDATE
-# -------------------------
-@app.route('/edit/<int:id>', methods=['GET', 'POST'])
-def edit(id):
+# Complete Task
+@app.route('/complete/<int:id>')
+def complete(id):
     task = Todo.query.get_or_404(id)
+    task.completed = not task.completed
 
-    if request.method == 'POST':
-        task.task = request.form['task']
-        db.session.commit()
-        return redirect(url_for('index'))
+    db.session.commit()
+    return redirect('/')
 
-    return render_template('edit.html', task=task)
-
-# -------------------------
-# DELETE
-# -------------------------
+# Delete Task
 @app.route('/delete/<int:id>')
 def delete(id):
     task = Todo.query.get_or_404(id)
@@ -66,21 +56,19 @@ def delete(id):
     db.session.delete(task)
     db.session.commit()
 
-    return redirect(url_for('index'))
+    return redirect('/')
 
-# -------------------------
-# COMPLETE FEATURE
-# -------------------------
-@app.route('/complete/<int:id>')
-def complete(id):
+# Edit Task
+@app.route('/edit/<int:id>', methods=['POST'])
+def edit(id):
     task = Todo.query.get_or_404(id)
+    task.task = request.form['task']
 
-    task.completed = not task.completed
     db.session.commit()
+    return redirect('/')
 
-    return redirect(url_for('index'))
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
 
-# -------------------------
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
-
+    app.run(debug=True, host="0.0.0.0")
